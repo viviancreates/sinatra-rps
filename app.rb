8079b867c5dbb94f35c8c422d826a2a13ccb7d47
viviancreates <168599473+viviancreates@users.noreply.github.com>
2024-09-10 05:05:56 +0000

    Edits with video

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+# run bin/server in terminal 
+
 require "sinatra"
 require "sinatra/reloader"
 
@@ -6,4 +8,29 @@ get("/") do
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+
+erb(:zebra)
+
+end
+
+get("/paper") do
+
+end
+
+get("/scissors") do
+
 end
